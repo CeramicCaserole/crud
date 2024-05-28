c06d67b6696fe06e685baa97ca319da757e9044c
CeramicCaserole <163989157+CeramicCaserole@users.noreply.github.com>
2024-05-28 23:43:55 +0800

    Create fireConfig.js

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDt2O5QP0eXmX7y1lnEpzH4TrNmBsy7wVI",
+  authDomain: "crud-286d6.firebaseapp.com",
+  databaseURL: "https://crud-286d6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-286d6",
+  storageBucket: "crud-286d6.appspot.com",
+  messagingSenderId: "715190359349",
+  appId: "1:715190359349:web:bd21cd8d937b01cd49687c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
